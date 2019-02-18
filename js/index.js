d24c6773fa74168f63c209fe90cdfacac74ca629
Nandor Naszradi <nandor_naszradi@epam.com>
2019-02-18 16:34:49 +0100

    list function updates

--- js/index.js
+++ js/index.js
@@ -1,18 +1,30 @@
 "use strict"
 
-var list = () => {
+var list = (condition) => {
     todoList.forEach((listItem, index) => {
-
-        let hours = listItem.dateTime.getHours(),
+        let timeAndTitle,
+            hours = listItem.dateTime.getHours(),
             minutes = listItem.dateTime.getMinutes();
         //Time formatting
         hours = hours < 10 ? '0' + hours : hours;
         minutes = minutes < 10 ? '0' + minutes : minutes;
-
-        console.log((index + 1) + '. List item: ' + JSON.stringify(hours + ':' + minutes + ' - ' + listItem.title));
+        //Time & title
+        timeAndTitle = hours + ':' + minutes + ' - ' + listItem.title;
+        //List the items by priorities, urgency
+        if (condition === 'all') {
+            console.log((index + 1) + '. List item: ' + JSON.stringify(timeAndTitle));
+        } else if (condition === 'high' && listItem.priority === 'high') {
+            console.log((index + 1) + '. High priority task: ' + JSON.stringify(timeAndTitle));
+        } else if (condition === 'normal' && listItem.priority === 'normal') {
+            console.log((index + 1) + '. Normal priority task: ' + JSON.stringify(timeAndTitle));
+        } else if (condition === 'low' && listItem.priority === 'low') {
+            console.log((index + 1) + '. Low priority task: ' + JSON.stringify(timeAndTitle));
+        } else if (condition === 'urgent' && listItem.urgent === true) {
+            console.log((index + 1) + '. Urgency task: ' + JSON.stringify(timeAndTitle));
+        }
     });
 };
 
 document.addEventListener('DOMContentLoaded', event => {
-    list();
+    //list();
 });
